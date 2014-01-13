475e1dcddd87ab4745d3d109b4ca59bee8b0f4d3
SeungyoupBaek <bsy8220@naver.com>
2014-01-13 10:46:14 +0900

    텝바 컨트롤러 실습 완료했습니다.

--- TabBarController/FirstViewController.m
+++ TabBarController/FirstViewController.m
@@ -7,12 +7,32 @@
 //
 
 #import "FirstViewController.h"
+#import "AppDelegate.h"
 
-@interface FirstViewController ()
+@interface FirstViewController () <UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *textField;
 
 @end
 
 @implementation FirstViewController
+
+// 엔터치면 키보드 감추기
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+// 편집 상태에서 완료될 때, 입력값을 공유 데이터에 저장
+-(void)textFieldDidEndEditing:(UITextField *)textField{
+    AppDelegate *delegate = [UIApplication sharedApplication].delegate;
+    delegate.sharedStr = textField.text;
+}
+
+// 나타날 때에는 공유 데이터에서 값을 얻어와서 뷰를 갱신한다.
+-(void)viewWillAppear:(BOOL)animated{
+    AppDelegate *delegate = [UIApplication sharedApplication].delegate;
+    self.textField.text = delegate.sharedStr;
+}
 
 - (void)viewDidLoad
 {
--- TabBarController/SecondViewController.m
+++ TabBarController/SecondViewController.m
@@ -7,12 +7,30 @@
 //
 
 #import "SecondViewController.h"
-
-@interface SecondViewController ()
+#import "AppDelegate.h"
+@interface SecondViewController () <UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *textField;
 
 @end
 
 @implementation SecondViewController
+// 엔터를 치면 키보드 감추기
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+// 편집 상태에서 완료될때, 입력값을 공유 데이터에 저장
+-(void)textFieldDidEndEditing:(UITextField *)textField{
+    AppDelegate *delegate = [UIApplication sharedApplication].delegate;
+    delegate.sharedStr = textField.text;
+}
+
+// 나타날 때에는 공유 데이터에서 값을 얻어와서 뷰에 갱신한다.
+-(void)viewWillAppear:(BOOL)animated{
+    AppDelegate *delegate = [UIApplication sharedApplication].delegate;
+    self.textField.text = delegate.sharedStr;
+}
 
 - (void)viewDidLoad
 {
